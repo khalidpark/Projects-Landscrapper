85eea2c197e361b34bd83f65cebb5160cd00ae4b
khalidpark2029 <yallayallakorea@gmail.com>
2021-01-28 04:53:57 +0000

    super hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, request, redirect, send_file
-from scrapper import get_jobs
-from exporter import save_to_file
+from flask import Flask, render_template, request, redirect
+from scrapper import extract_land
 
 app = Flask("Man_Seok_Kkun")
 
@@ -10,19 +9,23 @@ db = {}
 def home():
   return render_template("home.html")
 
-@app.route("/report")
-def report():
-  word = request.args.get('word')
-  if word:
-    word = word.lower()
-    existingJobs = db.get(word)
-    if existingJobs:
-      jobs = existingJobs
-    else : 
-      jobs = get_jobs(word)
-      db[word] = jobs
-  else :
-    return redirect("/")
-  return render_template("report.html", searchingBy = word, resultsNumber=len(jobs), jobs=jobs)
+@app.route("/test")
+def test():
+  return extract_land('https://new.land.naver.com/offices?ms=37.7668564,126.7116667,17&a=TJ&b=A1&e=RETAIL&g=10000&ad=true')
+
+#@app.route("/report")
+#def report():
+#  word = request.args.get('word')
+#  if word:
+#    word = word.lower()
+#    existingLands = db.get(word)
+#    if existingLands:
+#      lands = existingLands
+#    else : 
+#      lands = get_lands(word)
+#      db[word] = lands
+#  else :
+#    return redirect("/")
+#  return render_template("report.html", searchingBy = word, resultsNumber=len(jobs), jobs=jobs)
 
 app.run(host="0.0.0.0")
--- scrapper.py
+++ scrapper.py
@@ -1,14 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
-def extract_land(html):
-#  result = html.find('div', {'class' : 'fl1'})
-  title = html.find('h2').find('a')['title']
-  company, location = html.find('h3').find_all('span', recrusive=False)
-  company = company.get_text(strip=True).strip("-")
-  location = location.get_text(strip=True).strip("-")
-  job_id = html['data-jobid']
-  return {'title':title, 'company':company, 'location':location, "apply_link" : f"https://stackoverflow.com/jobs/{job_id}"}
+def extract_land(url):
+  title = url.find('div',{'class' : 'item_title'}).find_all('span', recrusive=False)
+  price = url.find('div',{'class' : 'price_line'}).find('span',{'class' : 'price'})
+  #price = price2.find('span',{'class' : 'price'})
+  price = price.get_text(strip=True)
+  price = float(price)
+  print(price)
+
+  landsize, details = url.find('div',{'class' : 'info_area'}).find_all('span', recrusive=False)
+  landsize = landsize.get_text(strip=True)
+  print(landsize)
+
+  #company, location = html.find('h3').find_all('span', recrusive=False)
+
+  #location = location.get_text(strip=True).strip("-")
+  #print(location)
+
+  #company, location = html.find('h3').find_all('span', recrusive=False)
+  #company = company.get_text(strip=True).strip("-")
+  #location = location.get_text(strip=True).strip("-")
+  #job_id = html['data-jobid']
+  return {'title':title, 'price':price, 'landsize':landsize}
 
 def extract_lands(last_page, url):
   lands = []
@@ -22,7 +36,7 @@ def extract_lands(last_page, url):
       lands.append(land)
     return lands
 
-def get_lands():
-    url = "https://new.land.naver.com/offices?ms=37.7668564,126.7116667,17&a=TJ&b=A1&e=RETAIL&g=10000&ad=true"
-    lands = extract_lands(url)
-    return lands
+#def get_lands():
+#    url = "https://new.land.naver.com/offices?ms=37.7668564,126.7116667,17&a=TJ&b=A1&e=RETAIL&g=10000&ad=true"
+#    lands = extract_lands(url)
+#    return lands
