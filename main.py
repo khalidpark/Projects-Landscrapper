ac18a91705c7a7cb0db53af97034811b65ce97a0
khalidpark2029 <yallayallakorea@gmail.com>
2021-01-29 14:29:24 +0000

    네이버부동산, 외부 비정상적인 접근 감지로 인한 서비스 이용제한으로 확인. 다른 사이트로 다시 시도 해보기

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-from flask import Flask, render_template, request, redirect
-from scrapper import extract_land
+#from flask import Flask, render_template, request, redirect
+#from scrapper import extract_land
+from scrapper import get_lands
 
-app = Flask("Man_Seok_Kkun")
+get_lands = get_lands()
 
-db = {}
 
-@app.route("/")
-def home():
-  return render_template("home.html")
 
-@app.route("/test")
-def test():
-  return extract_land('https://new.land.naver.com/offices?ms=37.7668564,126.7116667,17&a=TJ&b=A1&e=RETAIL&g=10000&ad=true')
+
+#app = Flask("Man_Seok_Kkun")
+
+#db = {}
+
+#@app.route("/")
+#def home():
+#  return render_template("home.html")
+
+#@app.route("/test")
+#def test():
+#  return extract_land('https://new.land.naver.com/offices?ms=37.7668564,126.7116667,17&a=TJ&b=A1&e=RETAIL&g=10000&ad=true')
 
 #@app.route("/report")
 #def report():
@@ -28,4 +34,4 @@ def test():
 #    return redirect("/")
 #  return render_template("report.html", searchingBy = word, resultsNumber=len(jobs), jobs=jobs)
 
-app.run(host="0.0.0.0")
+#app.run(host="0.0.0.0")
--- scrapper.py
+++ scrapper.py
@@ -1,17 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 
-def extract_land(url):
-  title = url.find('div',{'class' : 'item_title'}).find_all('span', recrusive=False)
-  price = url.find('div',{'class' : 'price_line'}).find('span',{'class' : 'price'})
-  #price = price2.find('span',{'class' : 'price'})
-  price = price.get_text(strip=True)
-  price = float(price)
-  print(price)
+URL = f'https://new.land.naver.com/offices?ms=37.7668564,126.7116667,17&a=TJ&b=A1&e=RETAIL&g=10000&ad=true'
 
-  landsize, details = url.find('div',{'class' : 'info_area'}).find_all('span', recrusive=False)
-  landsize = landsize.get_text(strip=True)
-  print(landsize)
+def get_current_page():
+  result = requests.get(URL)
+  soup = BeautifulSoup(result.text, "html.parser")
+  print(soup)
+  pagination = soup.find("div", {"class" : "item_title"})
+  print(pagination)
+
+def get_lands():
+    current_page = get_current_page()
+
+#def extract_land(url):
+#  title = url.find('div',{'class' : 'item_title'}).find_all('span', recrusive=False)
+#  price = url.find('div',{'class' : 'price_line'}).find('span',{'class' : 'price'})
+  #price = price2.find('span',{'class' : 'price'})
+#  price = price.get_text(strip=True)
+#  price = float(price)
+#  print(price)
+
+#  landsize, details = url.find('div',{'class' : 'info_area'}).find_all('span', recrusive=False)
+#  landsize = landsize.get_text(strip=True)
+#  print(landsize)
 
   #company, location = html.find('h3').find_all('span', recrusive=False)
 
@@ -22,19 +34,19 @@ def extract_land(url):
   #company = company.get_text(strip=True).strip("-")
   #location = location.get_text(strip=True).strip("-")
   #job_id = html['data-jobid']
-  return {'title':title, 'price':price, 'landsize':landsize}
+#  return {'title':title, 'price':price, 'landsize':landsize}
 
-def extract_lands(last_page, url):
-  lands = []
-  for page in range(last_page):
-    print(f"Scrapping Stackoverflow : page : {page}")
-    result = requests.get(f"{url}&pg=page{page+1}")
-    soup = BeautifulSoup(result.text, "html.parser")
-    results = soup.find_all("div", {"class":"-job"})
-    for result in results:
-      land = extract_land(result)
-      lands.append(land)
-    return lands
+#def extract_lands(last_page, url):
+#  lands = []
+#  for page in range(last_page):
+#    print(f"Scrapping Stackoverflow : page : {page}")
+#    result = requests.get(f"{url}&pg=page{page+1}")
+#    soup = BeautifulSoup(result.text, "html.parser")
+#    results = soup.find_all("div", {"class":"-job"})
+#    for result in results:
+#      land = extract_land(result)
+#      lands.append(land)
+#    return lands
 
 #def get_lands():
 #    url = "https://new.land.naver.com/offices?ms=37.7668564,126.7116667,17&a=TJ&b=A1&e=RETAIL&g=10000&ad=true"
